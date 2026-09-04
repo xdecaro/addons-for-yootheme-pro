efd34f84f018a9227c2824983673829491ad4ab5
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-04 22:26:22 +0200

    release: bump asset version to 1.0.7

--- modules/loadmore/src/AssetsListener.php
+++ modules/loadmore/src/AssetsListener.php
@@ -5,7 +5,7 @@ use YOOtheme\Path;
 
 final class LoadMoreYoothemeAssetsListener
 {
-    private const VERSION = '1.0.6';
+    private const VERSION = '1.0.7';
 
     public static function initHead(Metadata $metadata): void
     {
