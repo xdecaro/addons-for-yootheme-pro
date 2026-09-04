84744ef82dd3a1905ad731230fad20ec332a6a30
xdecaro <yvv4jfzkbb@privaterelay.appleid.com>
2026-09-04 18:52:51 +0200

    feat: load versioned assets

---
+++ modules/loadmore/src/AssetsListener.php
@@ -0,0 +1,21 @@
+<?php
+
+use YOOtheme\Metadata;
+use YOOtheme\Path;
+
+final class LoadMoreYoothemeAssetsListener
+{
+    private const VERSION = '1.0.2';
+
+    public static function initHead(Metadata $metadata): void
+    {
+        $metadata->set('style:load-more-yootheme', [
+            'href' => Path::get('../assets/css/loadmore.css') . '?v=' . self::VERSION,
+        ]);
+
+        $metadata->set('script:load-more-yootheme', [
+            'src' => Path::get('../assets/js/loadmore.js') . '?v=' . self::VERSION,
+            'defer' => true,
+        ]);
+    }
+}
